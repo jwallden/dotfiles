9e4fbc4708477e75c4b3cac1b18316468d734d95
Joakim Wallden <jocke.wallden@gmail.com>
2025-04-02 10:52:19 +0200

    More fiddling with workspace setup for different computers. Does not work exactly as i want on jonkin but good enough for now I guess.

--- hypr/.config/hypr/workspace_setup.sh
+++ hypr/.config/hypr/workspace_setup.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
+#!/bin/env bash
 
 HOST=$(hostname)
 
 if [[ $HOST == "jonkin" ]]; then
-  hyprctl keyword workspace 1,monitor:HDMI-A-1,default:true
-  hyprctl keyword workspace 2,monitor:HDMI-A-1
-  hyprctl keyword workspace 3,monitor:HDMI-A-1
-  hyprctl keyword workspace 4,monitor:HDMI-A-1
-  hyprctl keyword workspace 5,monitor:HDMI-A-1
-  hyprctl keyword workspace 6,monitor:HDMI-A-2,default:true
+  hyprctl keyword workspace 1, monitor:HDMI-A-1, default:true, persistent:true
+  hyprctl keyword workspace 2, monitor:HDMI-A-1, persistent:true
+  hyprctl keyword workspace 3, monitor:HDMI-A-1, persistent:true
+  hyprctl keyword workspace 10, monitor:HDMI-A-2, default:true, persistent:true
+  sleep 0.5
   hyprctl dispatch moveworkspacetomonitor 1 HDMI-A-1
   hyprctl dispatch moveworkspacetomonitor 2 HDMI-A-1
   hyprctl dispatch moveworkspacetomonitor 3 HDMI-A-1
-  hyprctl dispatch moveworkspacetomonitor 4 HDMI-A-1
-  hyprctl dispatch moveworkspacetomonitor 5 HDMI-A-1
-  hyprctl dispatch moveworkspacetomonitor 6 HDMI-A-2
+  hyprctl dispatch moveworkspacetomonitor 10 HDMI-A-2
 
-  hyprctl keyword bind SUPER,§,workspace,6
+  # hyprctl dispatch -- exec [workspace 10 silent] kitty --session tall-session &&
+  #   hyprctl dispatch workspace 1
+  # hyprctl keyword source ~/.config/hypr/workspaces_jonkin.conf
+
 fi
 
-if [[ $HOST == "pulse" ]]; then
-  echo $HOST
+if [[ $host == "pulse" ]]; then
+  hyprctl keyword workspace 1, monitor:eDP-1, default:true, persistent:true
+  hyprctl keyword workspace 2, monitor:eDP-1, persistent:true
+  hyprctl keyword workspace 3, monitor:eDP-1, persistent:true
 fi
